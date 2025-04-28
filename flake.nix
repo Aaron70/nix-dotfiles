2fcd60f1b99a1cf477af60e3af1a5bb3ee4ea9bc
aaronv <aarongeovanny70@gmail.com>
2025-04-27 20:53:00 -0600

    Creating initial folder structure

--- flake.nix
+++ flake.nix
@@ -16,15 +16,15 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."aaronv" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      homeConfigurations = {
+        aaronv = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+          # Specify your home configuration modules here, for example, the path to your home.nix.
+          modules = [ ./configuration/home/aaronv ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+          # Optionally use extraSpecialArgs to pass through arguments to home.nix
+        };
       };
     };
 }
