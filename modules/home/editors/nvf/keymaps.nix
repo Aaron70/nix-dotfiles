dfa6904a6d5e063100fae3b88c8fd4d02eb65923
aaronv <aarongeovanny70@gmail.com>
2025-04-28 23:36:13 -0600

    NVF: (Neovim) Add keybindings to move between splited panes

--- modules/home/editors/nvf/keymaps.nix
+++ modules/home/editors/nvf/keymaps.nix
@@ -1,13 +1,24 @@
 [
+  #==============================Others==============================#
   { mode = [ "i" ]; key = "jk"; action = "<esc>"; }
+  { key = "<Esc>"; action = "<cmd>nohlsearch<CR>"; mode = [ "n" ]; }
+  #==============================Others==============================#
 
   #==============================Clipboard==============================#
   { key = "<leader>y"; action = "\"+y"; mode = [ "v" ]; desc = "[Y]ank to clipboard"; }
   { key = "<leader>yy"; action = "\"+yy"; mode = [ "n" ]; desc = "[Y]ank line to clipboard"; }
   { key = "<leader>p"; action = "\"+p"; mode = [ "n" "v" ]; desc = "[P]aste from clipboard"; }
   { key = "<leader>P"; action = "\"+P"; mode = [ "n" "v" ]; desc = "[P]aste from clipboard (Before)"; }
-  #=key = =============================Clipboard==============================#
+  #==============================Clipboard==============================#
 
+
+
+  #==============================Movement==============================#
+  { key = "<C-h>"; action = "<C-w><C-h>"; mode = [ "n" ]; desc = "Move focus to the left pane"; }
+  { key = "<C-l>"; action = "<C-w><C-l>"; mode = [ "n" ]; desc = "Move focus to the right pane"; }
+  { key = "<C-j>"; action = "<C-w><C-j>"; mode = [ "n" ]; desc = "Move focus to the down pane"; }
+  { key = "<C-k>"; action = "<C-w><C-k>"; mode = [ "n" ]; desc = "Move focus to the up pane"; }
+  #==============================Movement==============================#
 
 
   # Plugins keymaps
