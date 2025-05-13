ee487dbd6497980e4ee1c4c3752d57b4d340c0fa
aaronv <aarongeovanny70@gmail.com>
2025-05-12 20:40:22 -0600

    NixOS: Enable pipewire

--- hosts/aaronv/configuration.nix
+++ hosts/aaronv/configuration.nix
@@ -101,14 +101,14 @@ in
     extraConfig = "load-module module-combine-sink";
   };
   security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  #   # To use JACK applications
-  #   # jack.enable = true;
-  # };
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # To use JACK applications
+    # jack.enable = true;
+  };
   # ====================|Display and DE|====================
 
 
