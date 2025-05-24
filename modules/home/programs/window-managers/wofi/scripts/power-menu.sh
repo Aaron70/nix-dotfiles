a87364ad3ae143a1bb24cc03164eea996d753d36
aaronv <aarongeovanny70@gmail.com>
2025-05-23 21:35:02 -0600

    Wofi: Add restart hyprland to power-menu

--- modules/home/programs/window-managers/wofi/scripts/power-menu.sh
+++ modules/home/programs/window-managers/wofi/scripts/power-menu.sh
@@ -3,9 +3,10 @@
 POWEROFF="Power Off"
 RESTART="Restart"
 LOGOUT="Log out"
+RELOAD="Restart Hyprland"
 
 # Define options
-options="$POWEROFF\n$RESTART\n$LOGOUT"
+options="$POWEROFF\n$RESTART\n$LOGOUT\n$RELOAD"
 
 # Show wofi menu and capture selection
 selection=$(echo -e "$options" | wofi --dmenu --insensitive --prompt "Select an option:")
@@ -20,5 +21,8 @@ case "$selection" in
         ;;
     "$LOGOUT")
         hyprctl dispatch exit
+        ;;
+    "$RELOAD")
+        hyprctl reload
         ;;
 esac
