282cc65eca1f2c1be0eefff05ac9e08122022ab3
aaronv <aarongeovanny70@gmail.com>
2025-08-06 20:07:18 -0600

    NixOS: move bootloader config to common module

--- hosts/aaronv/configuration.nix
+++ hosts/aaronv/configuration.nix
@@ -3,23 +3,8 @@
   imports = [ 
     ./hardware-configuration.nix
     ../bluetooth.nix
+    ../boot.nix
   ];
-
-  # ====================|Bootloader|====================
-  boot.loader.systemd-boot = {
-    enable = true;
-    # Custom entry for windows in the boot menu
-    # windows."Windows" = {
-    #   efiDeviceHandle = "FS0";
-    #   title = "Windows 11";
-    #   sortKey = "0_windows";
-    # };
-    configurationLimit = 7;
-  };
-  boot.loader.efi.canTouchEfiVariables = true;
-  # ====================|Bootloader|====================
-
-
 
   source-persisted-configs.enable = true; 
 }
--- hosts/laptop-aaronv/configuration.nix
+++ hosts/laptop-aaronv/configuration.nix
@@ -3,23 +3,8 @@ with lib; {
   imports = [ 
     ./hardware-configuration.nix
     ../bluetooth.nix
+    ../boot.nix
   ];
-
-  # ====================|Bootloader|====================
-  boot.loader.systemd-boot = {
-    enable = true;
-    # Custom entry for windows in the boot menu
-    # windows."Windows" = {
-    #   efiDeviceHandle = "FS0";
-    #   title = "Windows 11";
-    #   sortKey = "0_windows";
-    # };
-    configurationLimit = 7;
-  };
-  boot.loader.efi.canTouchEfiVariables = true;
-  # ====================|Bootloader|====================
-
-
 
   services.displayManager.sddm.enable = mkForce false;
   services.xserver.displayManager.gdm.enable = mkForce true;
